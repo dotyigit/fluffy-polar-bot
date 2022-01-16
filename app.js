41edf9c323a754d7c2bc48715c11ee6f2914e43e
Ömer Yiğit Aker <omer@aker.pw>
2022-01-16 22:56:10 +0300

    🤩 looksrare integration

--- app.js
+++ app.js
@@ -20,7 +20,50 @@ client.on('ready', async () => {
 console.log("Fluffy Polar Bears Sales Bot Started Working...");
 
 
-function formatAndSendTweet(event) {
+const query = `
+query GetEventsQuery($pagination: PaginationInput, $filter: EventFilterInput) {
+    events(pagination: $pagination, filter: $filter) {
+      ...EventFragment
+    }
+  }
+  
+fragment EventFragment on Event {
+  id
+  from
+  to
+  type
+  hash
+  createdAt
+  token {
+    tokenId
+    image
+    name
+  }
+  collection {
+    address
+    name
+    description
+    totalSupply
+    logo
+    floorOrder {
+      price
+    }
+  }
+  order {
+    isOrderAsk
+    price
+    endTime
+    currency
+    strategy
+    status
+    params
+  }
+}`
+
+
+
+
+function formatAndSendTweetOpensea(event) {
     // Handle both individual items + bundle sales
     const assetName = _.get(event, ['asset', 'name'], _.get(event, ['asset_bundle', 'name']));
     const openseaLink = _.get(event, ['asset', 'permalink'], _.get(event, ['asset_bundle', 'permalink']));
@@ -35,7 +78,7 @@ function formatAndSendTweet(event) {
     const formattedEthPrice = formattedUnits * tokenEthPrice;
     const formattedUsdPrice = formattedUnits * tokenUsdPrice;
 
-    const tweetText = `${assetName} just sold! Price: ${ethers.constants.EtherSymbol}${formattedEthPrice} ($${Number(formattedUsdPrice).toFixed(2)}) #NFT #FPBears ${openseaLink}`;
+    const tweetText = `${assetName} bought on @OpenSea for ${ethers.constants.EtherSymbol}${formattedEthPrice} ($${Number(formattedUsdPrice).toFixed(2)}) #NFT #FPBears ${openseaLink}`;
     const channel = client.channels.cache.find(channel => channel.id === '921446553338658876')
     // TEST CHANNEL BELOW
     // const channel = client.channels.cache.find(channel => channel.id === '911572120721559572')
@@ -43,23 +86,46 @@ function formatAndSendTweet(event) {
     
     channel.send(tweetText);
 
-    // OPTIONAL PREFERENCE - don't tweet out sales below X ETH (default is 1 ETH - change to what you prefer)
-    // if (Number(formattedEthPrice) < 1) {
-    //     console.log(`${assetName} sold below tweet price (${formattedEthPrice} ETH).`);
-    //     return;
-    // }
-
-    // OPTIONAL PREFERENCE - if you want the tweet to include an attached image instead of just text
-    // const imageUrl = _.get(event, ['asset', 'image_url']);
-    // return tweet.tweetWithImage(tweetText, imageUrl);
-
     return tweet.tweet(tweetText);
+}
+
+
+function formatAndSendTweetLooksrare(event) {
+    const assetName = _.get(event, ['token', 'name']);
+    const looksRareLink = "https://looksrare.org/collections/" + _.get(event, ['collection', 'address']) + "/" +_.get(event, ['token', 'tokenId']);
+
+    const price = _.get(event, ['order', 'price']);
+
+    const formattedEthPrice = ethers.utils.formatEther(price);
+
+    axios.get('https://api.coinbase.com/v2/prices/ETH-USD/spot').then((response) => {
+        const ethResponse = response
+    
+        const ethUsdPrice = _.get(ethResponse, ['data', 'data', 'amount'], 0)
+    
+        if (ethUsdPrice) {
+    
+            const formattedUsdPrice = formattedEthPrice * ethUsdPrice
+            
+            const tweetText = `${assetName} bought on @LooksRareNFT for ${ethers.constants.EtherSymbol}${formattedEthPrice} ($${Number(formattedUsdPrice).toFixed(2)}) #NFT #FPBears ${looksRareLink}`;
+            const channel = client.channels.cache.find(channel => channel.id === '921446553338658876')
+            // TEST CHANNEL BELOW
+            // const channel = client.channels.cache.find(channel => channel.id === '911572120721559572')
+            console.log(tweetText);
+            
+            channel.send(tweetText);
+        
+        
+            return tweet.tweet(tweetText);
+        }
+    })
+    
 }
 
 
 setInterval(() => {
     const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").unix();
-    console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
+    //console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
 
     axios.get('https://api.opensea.io/api/v1/events', {
         headers: {
@@ -79,19 +145,64 @@ setInterval(() => {
 
             return new Date(created);
         })
-
-        console.log(`${events.length} sales since the last one...`);
+        
+        console.log(`Opensea: ${events.length} sales since the last one...`);
 
         _.each(sortedEvents, (event) => {
             const created = _.get(event, 'created_date');
 
             cache.set('lastSaleTime', moment(created).unix());
 
-            return formatAndSendTweet(event);
+            return formatAndSendTweetOpensea(event);
         });
     }).catch((error) => {
         console.error(error);
     });
+
+
+    // LOOKSRARE
+    const lastSaleTimeAtLooksrare = cache.get('lastSaleTimeAtLooksrare', null) || 1642335726;
+
+    axios.post('https://api.looksrare.org/graphql', {
+        query: query,
+        variables: {
+            "filter": {
+              "collection": "0xDebBC3691d42090d899Cafe0C4ED80897A7C9d6a",
+              "type": "SALE"
+            },
+            "pagination": {
+              "first": 20
+            }
+          }
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+    }).then((response) => {
+        
+        const events = _.get(response, ['data', 'data', 'events']);
+    
+        const filteredEvents = _.filter(events, function(event) {
+            return moment(event.createdAt).unix() > lastSaleTimeAtLooksrare
+        })
+    
+        console.log(`LooksRare: ${filteredEvents.length} sales since the last one...`);
+    
+        _.each(filteredEvents, (event) => {
+            const created = _.get(event, 'createdAt');
+    
+            cache.set('lastSaleTimeAtLooksrare', moment(created).unix());
+
+            return formatAndSendTweetLooksrare(event);
+    
+        });
+    }).catch((err) => {
+        console.error(err);
+    })
+
+
+
 
 }, 60000);
 
